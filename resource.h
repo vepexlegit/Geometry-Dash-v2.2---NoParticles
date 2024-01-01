f5f6a9384be347b533a8a86f38464fab1689e618
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2024-01-01 20:06:55 +0400

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется Geometry Dash - NoParticles.rc
+// Р’РєР»СЋС‡Р°РµРјС‹Р№ С„Р°Р№Р», СЃРѕР·РґР°РЅРЅС‹Р№ РІ Microsoft Visual C++.
+// РСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ Geometry Dash - NoParticles.rc
 //
 #define IDI_ICON1                       101
 
